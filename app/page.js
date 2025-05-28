b17b4722adc291106dbb43ba6e2d45318c80bbb2
veerrai <karamveer20000625@gmail.com>
2025-05-28 12:28:32 +0530

    dymaic blog page created

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 
-import Header from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
+// import Header from "@/components/common/Header";
+// import Footer from "@/components/common/Footer";
 import "./globals.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from "axios";
-import MediumCard from "@/components/common/MediumCard";
-import { HOST } from "@/utils/static";
-import RecentNews from "@/components/common/RecentNews";
-import AllCategory from "@/components/common/AllCategory";
+// import axios from "axios";
+// import MediumCard from "@/components/common/MediumCard";
+// import { HOST } from "@/utils/static";
+// import RecentNews from "@/components/common/RecentNews";
+// import AllCategory from "@/components/common/AllCategory";
 
 
 export default async function Home() {
