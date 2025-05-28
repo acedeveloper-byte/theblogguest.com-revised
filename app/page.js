97765eeb19c2b62827958a92e69b0a6521499fe6
veerrai <karamveer20000625@gmail.com>
2025-05-28 13:30:46 +0530

    code push

--- app/page.js
+++ app/page.js
@@ -5,25 +5,23 @@ import "./globals.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import MediumCard from "@/components/common/MediumCard";
-import { HOST } from "@/utils/static";
 import RecentNews from "@/components/common/RecentNews";
 import AllCategory from "@/components/common/AllCategory";
 
 
 export default async function Home() {
 
-  // const travel = await axios.get(`${HOST}post/fetch-all-post-by-category/travel`);
-  // const travelresp = travel.data?.response || [];
+
 
   return (
     <>
 
-      {/* <Header />
+      <Header />
 
       <RecentNews />
       <AllCategory />
 
-      <Footer /> */}
+      <Footer />
 
 
 
